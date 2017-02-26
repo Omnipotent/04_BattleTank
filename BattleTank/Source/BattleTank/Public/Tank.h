c28500706e8cbf1f778f84326d2e7aca5eca65dd
Nicholas Wilkie <io2red@hotmail.com>
2017-02-26 14:43:00 -0800

    BT33 - SuggestProjectileVelocity()

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -20,6 +20,9 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
 
+	UPROPERTY(EditAnywhere, Category = Firing)
+	float LaunchSpeed = 100000; // TODO Find sensible default LaunchSpeed
+
 protected:
 	UTankAimingComponent* TankAimingComponent = nullptr;
 
