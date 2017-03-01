d324df2bd6b2312ba7dd615d2099d513d1efbc01
Nicholas Wilkie <io2red@hotmail.com>
2017-02-28 16:45:27 -0800

    BT47 - Projectile Movement Components

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -14,14 +14,15 @@ public:
 	// Sets default values for this actor's properties
 	AProjectile();
 
+	void LaunchProjectile(float Speed);
+		
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UProjectileMovementComponent* ProjectileMovement = nullptr;
 
-	
-	
 };
