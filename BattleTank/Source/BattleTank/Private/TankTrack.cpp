9877b70bd3f7274f78de9115e014901031ca7ef3
Nicholas Wilkie <io2red@hotmail.com>
2017-03-04 11:13:24 -0800

    BT51 - Setup Track Throttles

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	//auto Time = GetWorld()->GetTimeSeconds();
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s track throttle: %f"), *Name, Throttle);
+
+	// TODO Clamp actual throttle value so player cannot change throttle power
+}
