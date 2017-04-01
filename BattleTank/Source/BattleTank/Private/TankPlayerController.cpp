bffa4c0b873744afa37e9236333666878acf8976
Nicholas Wilkie <io2red@hotmail.com>
2017-04-01 12:06:16 -0700

    BT93 - Bonus - Switching Cameras

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -81,7 +81,7 @@ bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVec
 			HitResult,
 			StartLocation,
 			EndLocation,
-			ECollisionChannel::ECC_Visibility)
+			ECollisionChannel::ECC_Camera)
 		)
 	{
 		// Set hit location
